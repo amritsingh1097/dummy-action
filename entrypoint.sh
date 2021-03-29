4e07e0ea90adf9993561cdfde279747054bdf622
Amrit Singh <amrit.1097@gmail.com>
2021-03-29 15:57:09 +0530

    13th commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,187 @@
 #!/bin/sh
 
 # Exit if a command exits with non-zero status
-# set -e
+set -e
 
 # Prints command and args as they execute
 set -x
 
 # Error if variable is unset when using
-# set -u
+set -u
 
-echo ::group::Logs
 
-echo "Added group------"
 
-echo "Script starts---"
+# ****************************************************************************************************
+# ****************************************************************************************************
+# ****************************************  Parameter Check  *****************************************
+# ****************************************************************************************************
+# *********************************************  Start  **********************************************
+# ****************************************************************************************************
+# ****************************************************************************************************
+# ****************                                                                   *****************
+# ****************    Check for the mandatory parameters and optional parameters.    *****************
+# ****************    If any mandatory parameter is missing then terminate the       *****************
+# ****************    script with non-zero code. If any optional parameter is        *****************
+# ****************    missing then assign the default value to those parameters.     *****************
+# ****************                                                                   *****************
+# ****************************************************************************************************
+# ****************************************************************************************************
 
-echo "$INPUT_SOURCE_DIR"
+# Mandatory parameter check - Start
+if [ -z $INPUT_SOURCE_BRANCH ] then
+    echo "Source branch must be specified"
+    return -1
+fi
 
-echo "$INPUT_TARGET_REPO"
+if [ -z $INPUT_TARGET_REPO ] then
+    echo "Target repo must be specified"
+    return -1
+fi
 
-echo "$INPUT_TARGET_BRANCH"
+if [ -z $INPUT_USER_EMAIL ] then
+    echo "Email must be specified"
+    return -1
+fi
 
-echo "Script ends"
+if [ -z $INPUT_USER_NAME ] then
+    echo "Username must be specified"
+    return -1
+fi
+# Mandatory parameter check - End
 
-echo ::endgroup::
+
+# Assign values to optional parameters - Start
+if [ -z $INPUT_TARGET_BRANCH ] then
+    $INPUT_TARGET_BRANCH="main"
+fi
+
+if [ -z $INPUT_COMMIT_MESSAGE ] then
+    $INPUT_COMMIT_MESSAGE="Application deployed by user $INPUT_USER_NAME from https://github.com/${GITHUB_REPOSITORY}.git using the commit ${GITHUB_SHA}"
+fi
+
+if [ -z $INPUT_DELETE_HISTORY ] then
+    $INPUT_DELETE_HISTORY=false
+fi
+# Assign values to optional parameters - End
+
+# ****************************************************************************************************
+# ****************************************************************************************************
+# ****************************************  Parameter Check  *****************************************
+# ****************************************************************************************************
+# **********************************************  End  ***********************************************
+# ****************************************************************************************************
+# ****************************************************************************************************
+
+
+
+# ****************************************************************************************************
+# ****************************************************************************************************
+# *********************************  Build the Angular application  **********************************
+# ****************************************************************************************************
+# *********************************************  Start  **********************************************
+# ****************************************************************************************************
+# ****************************************************************************************************
+# ****************                                                                   *****************
+# ****************    Check for the mandatory parameters and optional parameters.    *****************
+# ****************    If any mandatory parameter is missing then terminate the       *****************
+# ****************    script with non-zero code. If any optional parameter is        *****************
+# ****************    missing then assign the default value to those parameters.     *****************
+# ****************                                                                   *****************
+# ****************************************************************************************************
+# ****************************************************************************************************
+
+# Create a temporary directory for cloning the source and target repos
+CLONE_REPO=$(mktemp -d)
+
+# Store the source repo path
+SOURCE_REPO="$CLONE_REPO/source"
+
+# Create source repo inside CLONE_REPO
+cd $CLONE_REPO
+mkdir -p $SOURCE_REPO
+
+ls -al
+echo $SOURCE_REPO
+
+# Clone the source github repo - Start
+git config user.email "$INPUT_USER_EMAIL"
+git config user.name "$INPUT_USER_NAME"
+git clone --single-branch --branch $INPUT_SOURCE_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$GITHUB_REPOSITORY.git" "$SOURCE_REPO"
+# Clone the source github repo - End
+
+
+# Build the Angular app - Start
+cd $SOURCE_REPO
+npm install
+npm run build -- --output-path dist/
+# Build the Angular app - End
+
+# ****************************************************************************************************
+# ****************************************************************************************************
+# *********************************  Build the Angular application  **********************************
+# ****************************************************************************************************
+# **********************************************  End  ***********************************************
+# ****************************************************************************************************
+# ****************************************************************************************************
+
+
+
+# ****************************************************************************************************
+# ****************************************************************************************************
+# ****************************  Copy distribution bundle to target repo  *****************************
+# ****************************************************************************************************
+# *********************************************  Start  **********************************************
+# ****************************************************************************************************
+# ****************************************************************************************************
+# ****************                                                                   *****************
+# ****************    Check for the mandatory parameters and optional parameters.    *****************
+# ****************    If any mandatory parameter is missing then terminate the       *****************
+# ****************    script with non-zero code. If any optional parameter is        *****************
+# ****************    missing then assign the default value to those parameters.     *****************
+# ****************                                                                   *****************
+# ****************************************************************************************************
+# ****************************************************************************************************
+
+# Store the target repo path
+TARGET_REPO="$CLONE_REPO/target"
+echo $TARGET_REPO
+
+# Create target repo inside CLONE_REPO
+cd $CLONE_REPO
+mkdir -p $TARGET_REPO
+
+# Clone the target github repo - Start
+git clone --single-branch --branch $INPUT_TARGET_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_TARGET_REPO.git" "$TARGET_REPO"
+cd $TARGET_REPO
+
+
+# If $INPUT_DELETE_HISTORY is set to 'true' then reset the .git folder and initialize it again
+if [ $INPUT_DELETE_HISTORY ] then
+    echo "Deleting history"
+    rm -rf .git/
+    git init
+    git config user.email "$INPUT_USER_EMAIL"
+    git config user.name "$INPUT_USER_NAME"
+    git branch -M "$INPUT_TARGET_BRANCH"
+    git remote add origin "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_TARGET_REPO.git"
+    echo "History deleted"
+fi
+# Clone the target github repo - End
+
+
+# Copy the contents from SOURCE_REPO/dist to TARGET_REPO
+cp -r $SOURCE_REPO/dist $TARGET_REPO/
+echo "bundle copied"
+ls -al
+# Push the repo to github
+# git add .
+# git commit -m "$INPUT_COMMIT_MESSAGE"
+# git push -u origin "$INPUT_TARGET_BRANCH"
+
+# ****************************************************************************************************
+# ****************************************************************************************************
+# ****************************  Copy distribution bundle to target repo  *****************************
+# ****************************************************************************************************
+# **********************************************  End  ***********************************************
+# ****************************************************************************************************
+# ****************************************************************************************************
